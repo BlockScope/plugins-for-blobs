629bf5e6e45b1aa267de2dd26b8cdede9995509a
Phil de Joux <philderbeast@gmail.com>
2020-07-09 12:16:09 -0400

    Bring over more tests from uom-plugin.

--- thoralf/test-suite-units/Tests.hs
+++ thoralf/test-suite-units/Tests.hs
@@ -1,14 +1,24 @@
-{-# LANGUAGE TypeOperators, AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
 {-# OPTIONS_GHC -fplugin Plugins.Thoralf #-}
 
 module Main where
 
-import Test.Tasty
 import Data.UnitsOfMeasure
+import Test.Tasty
+import Test.Tasty.HUnit
 
 sum' :: [Quantity Double u] -> Quantity Double u
 sum' = foldr (+:) zero
+
+-- Check that the abelian group laws hold
+
+associativity :: Quantity a (u *: (v *: w)) -> Quantity a ((u *: v) *: w)
+associativity = id
 
 {- NOTE: I enabled AllowAmbiguousTypes to get commutativity to compile.
     • Couldn't match type ‘v0 *: u0’ with ‘v *: u’
@@ -27,8 +37,44 @@ commutativity = id
 unit :: Quantity a (u *: One) -> Quantity a u
 unit = id
 
+inverse :: Quantity a (u *: (One /: u)) -> Quantity a One
+inverse = id
+
+inverse2 :: proxy b -> Quantity a (Base b /: Base b) -> Quantity a One
+inverse2 _ = id
+
+-- u ~ v * w, v^2 ~ v  =>  u ~ w
+g :: (u ~ (v *: w), (v ^: 2) ~ v) => Quantity a u -> Quantity a w
+g = id
+
+
+{- NOTE: I enabled TypeFamilies to get givens to compile.
+    • Illegal equational constraint (a *: a) ~ One
+      (Use GADTs or TypeFamilies to permit this)
+    • In the type signature:
+        givens :: ((a *: a) ~ One) =>
+                  Quantity Double a -> Quantity Double One
+-}
+-- a*a ~ 1  =>  a ~ 1
+givens :: ((a *: a) ~ One) => Quantity Double a -> Quantity Double One
+givens = id
+
+-- NOTE: I enabled DataKinds to get givens2 to compile.
+--    Illegal type: ‘2’ Perhaps you intended to use DataKinds
+-- a^2 ~ b^3, b^6 ~ 1 => a ~ 1
+givens2 :: ((a ^: 2) ~ (b ^: 3), (b ^: 6) ~ One) => Quantity Double a -> Quantity Double One
+givens2 = id
+
+-- a^2 ~ b^3, b^37 ~ 1 => b ~ 1
+givens3 :: ((a ^: 2) ~ (b ^: 3), (b ^: 37) ~ One) => Quantity Double b -> Quantity Double One
+givens3 = id
+
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "uom-plugin" []
+tests = testGroup "uom-plugin"
+  [ testGroup "Get the underlying value with unQuantity"
+    [ testCase "unQuantity 1"                  $ unQuantity (mk 1)                @?= 1
+    ]
+  ]
