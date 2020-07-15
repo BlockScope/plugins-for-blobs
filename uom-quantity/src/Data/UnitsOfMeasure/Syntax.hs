6e51956d07ad3b97bbd3b89e3e3adf521c36190a
Phil de Joux <philderbeast@gmail.com>
2020-07-15 06:59:11 -0400

    Reflow wordwrap for haddocks of Syntax module.

--- uom-quantity/src/Data/UnitsOfMeasure/Syntax.hs
+++ uom-quantity/src/Data/UnitsOfMeasure/Syntax.hs
@@ -23,9 +23,9 @@ import Data.Theory.UoM
 
 infix 4 ~~
 
--- | Syntactic representation of a unit as a pair of lists of base
--- units, for example 'One' is represented as @[] ':/' []@ and
--- @'Base' "m" '/:' 'Base' "s" ^: 2@ is represented as @["m"] ':/' ["s","s"]@.
+-- | Syntactic representation of a unit as a pair of lists of base units, for
+-- example 'One' is represented as @[] ':/' []@ and @'Base' "m" '/:' 'Base' "s"
+-- ^: 2@ is represented as @["m"] ':/' ["s","s"]@.
 data UnitSyntax s = [s] :/ [s] deriving (Eq, Show)
 
 -- | Pack up a syntactic representation of a unit as a unit.  For example:
@@ -34,10 +34,9 @@ data UnitSyntax s = [s] :/ [s] deriving (Eq, Show)
 --
 -- @ 'Pack' (["m"] ':/' ["s","s"]) = 'Base' "m" '/:' 'Base' "s" ^: 2 @
 --
--- This is a perfectly ordinary closed type family.  'Pack' is a left
--- inverse of 'Unpack' up to the equational theory of units, but it is
--- not a right inverse (because there are multiple list
--- representations of the same unit).
+-- This is a perfectly ordinary closed type family.  'Pack' is a left inverse
+-- of 'Unpack' up to the equational theory of units, but it is not a right
+-- inverse (because there are multiple list representations of the same unit).
 type family Pack (u :: UnitSyntax Symbol) :: Unit where
     Pack (xs :/ ys) = Prod xs /: Prod ys
 
@@ -53,28 +52,27 @@ type family Exp (xs :: [(Symbol, Nat)]) :: Unit where
     Exp ('(u, 1) ': ys) = Base u *: Exp ys
     Exp ('(u, i) ': ys) = (Base u ^: i) *: Exp ys
 
--- | Unpack a unit as a syntactic representation, where the order of
--- units is deterministic.  For example:
+-- | Unpack a unit as a syntactic representation, where the order of units is
+-- deterministic.  For example:
 --
 -- @ 'Unpack' 'One' = [] ':/' [] @
 --
 -- @ 'Unpack' ('Base' "s" '*:' 'Base' "m") = ["m","s"] ':/' [] @
 --
--- This does not break type soundness because
--- 'Unpack' will reduce only when the unit is entirely constant, and
--- it does not allow the structure of the unit to be observed.  The
--- reduction behaviour is implemented by the plugin, because we cannot
--- define it otherwise.
+-- This does not break type soundness because 'Unpack' will reduce only when
+-- the unit is entirely constant, and it does not allow the structure of the
+-- unit to be observed.  The reduction behaviour is implemented by the plugin,
+-- because we cannot define it otherwise.
 type family Unpack (u :: Unit) :: UnitSyntax Symbol where {}
 
--- | This is a bit of a hack, honestly, but a good hack.  Constraints
--- @u ~~ v@ are just like equalities @u ~ v@, except solving them will
--- be delayed until the plugin.  This may lead to better inferred types.
+-- | This is a bit of a hack, honestly, but a good hack.  Constraints @u ~~ v@
+-- are just like equalities @u ~ v@, except solving them will be delayed until
+-- the plugin.  This may lead to better inferred types.
 type family (u :: Unit) ~~ (v :: Unit) :: Constraint where {}
 
--- | This type family is used for translating unit names (as
--- type-level strings) into units.  It will be 'Base' for base units
--- or expand the definition for derived units.
+-- | This type family is used for translating unit names (as type-level
+-- strings) into units.  It will be 'Base' for base units or expand the
+-- definition for derived units.
 --
 -- The instances displayed by Haddock are available only if
 -- "Data.UnitsOfMeasure.Defs" is imported.
