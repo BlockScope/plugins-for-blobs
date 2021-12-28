bc81efe64c7bc667892a386d1af6af1174a0ab89
Phil de Joux <philderbeast@gmail.com>
2021-12-28 14:50:39 -0500

    Indent the printing of SMT expressions.

--- thoralf/src/Plugins/Print/SMT.hs
+++ thoralf/src/Plugins/Print/SMT.hs
@@ -23,7 +23,7 @@ pprSmtInputs
     -> [SExpr]
     -> ShowS
 pprSmtInputs
-    indent@(Indent i)
+    iIndent@(Indent i)
     (TraceConvertCtsToSmt True)
     gSExprs
     wSExprs
@@ -31,54 +31,60 @@ pprSmtInputs
     = 
     ( tab
     . showString "sexpr-decs = "
-    . pprSmtList indent dSExprs
+    . pprSmtList jIndent dSExprs
     . showChar '\n'
     . tab
     . showString "sexpr-given = "
-    . pprSmtList indent gSExprs
+    . pprSmtList jIndent gSExprs
     . showChar '\n'
     . tab
     . showString "sexpr-wanted = "
-    . pprSmtList indent wSExprs
+    . pprSmtList jIndent wSExprs
     )
     where
         tab = showString $ replicate (2 * i) ' '
+        jIndent = iIndent + 1
 
 pprSmtInputs _ (TraceConvertCtsToSmt False) _ _ _ = const ""
 
 pprSmtList :: Indent -> [SExpr] -> ShowS
 pprSmtList _ [] = showString "[]"
-pprSmtList _ es =
-    showString "[\n"
+pprSmtList (Indent i) es = let tab = replicate (2 * i) ' ' in
+    showChar '['
+    . showChar '\n'
     . foldr
-        (\e m -> SMT.ppSExpr e . showChar '\n' . m)
-        (showChar ']')
+        (\e m -> showString tab . SMT.ppSExpr e . showChar '\n' . m)
+        (showString tab . showChar ']')
         es
 
 pprSmtGivens :: Indent -> [SExpr] -> ShowS
 pprSmtGivens _ [] = showString "[]"
-pprSmtGivens _ es =
-    showString "[\n"
+pprSmtGivens (Indent i) es = let tab = replicate (2 * i) ' ' in
+    showChar '['
+    . showChar '\n'
     . foldr
         (\e m ->
-            showString "(assert "
+            showString tab
+            . showString "(assert "
             . SMT.ppSExpr e
             . showChar ')'
             . showChar '\n'
             . m)
-        (showChar ']')
+        (showString tab . showChar ']')
         es
 
 pprSmtWanteds :: Indent -> [SExpr] -> ShowS
 pprSmtWanteds _ [] = showString "[]"
-pprSmtWanteds _ es =
-    showString "[\n"
+pprSmtWanteds (Indent i) es = let tab = replicate (2 * i) ' ' in
+    showChar '['
+    . showChar '\n'
     . foldr
         (\e m ->
-            showString "(assert (or false (not "
+            showString tab
+            . showString "(assert (or false (not "
             . SMT.ppSExpr e
             . showString ")))"
             . showChar '\n'
             . m)
-        (showChar ']')
+        (showString tab . showChar ']')
         es
