bf2be177088ccc70a77ee0037aff95fbb0049fad
Phil de Joux <philderbeast@gmail.com>
2022-01-04 13:49:28 -0500

    Typo in -i doc directories.

--- uom-plugin-tutorial/test-suite-doctest-ghc-8.2/DocTest.hs
+++ uom-plugin-tutorial/test-suite-doctest-ghc-8.2/DocTest.hs
@@ -4,7 +4,7 @@ import Test.DocTest (doctest)
 
 arguments :: [String]
 arguments =
-    [ "-idoc"
+    [ "-idoc-ghc-8.2"
     , "-isrc"
     , "./src/Plugins/UoM/UnitDefs.hs"
     , "./doc-ghc-8.2/Data/UnitsOfMeasure/Tutorial.hs"
