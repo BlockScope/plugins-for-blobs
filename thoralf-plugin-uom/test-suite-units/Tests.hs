e6c89b8505fa9ef3bafaf6eb3727117e3eb64b68
Phil de Joux <philderbeast@gmail.com>
2022-01-24 16:31:05 -0500

    Add attach units and basic operations test groups.

--- thoralf-plugin-uom/test-suite-units/Tests.hs
+++ thoralf-plugin-uom/test-suite-units/Tests.hs
@@ -88,6 +88,16 @@ main = defaultMain tests
 tests :: TestTree
 tests = testGroup "thoralf-plugin:units"
   [ testsUnQuantity
+  , testGroup "Attach units by applying the quasiquoter without a numeric value"
+    [ testCase "m 3"                           $ [u| m |] 3           @?= [u| 3 m |]
+    , testCase "m <$> [3..5]"                  $ [u| m |] <$> [3..5]  @?= [[u| 3 m |],[u| 4 m |],[u| 5 m |]]
+    , testCase "m/s 3"                         $ [u| m/s |] 3         @?= [u| 3 m/s |]
+    , testCase "m s^-1 3"                      $ [u| m s^-1 |] 3      @?= [u| 3 m s^-1 |]
+    , testCase "s^2 3"                         $ [u| s^2 |] 3         @?= [u| 3 s^2 |]
+    , testCase "1 $ 3"                         $ [u|dimensionless|] 3 @?= [u| 3 |]
+    , testCase "fmap [u| kg |] read $ \"3\""   $ readMass "3"         @?= [u| 3 kg |]
+    , testCase "fmap [u| kg |] read $ \"3.0\"" $ readMass "3"         @?= [u| 3.0 kg |]
+    ]
   , testGroup "Showing constants"
     [ testCase "show 3m"                 $ show [u| 3 m |]                @?= "[u| 3 m |]"
     , testCase "show 3m/s"               $ show [u| 3 m/s |]              @?= "[u| 3 m / s |]"
@@ -97,6 +107,12 @@ tests = testGroup "thoralf-plugin:units"
     , testCase "show 1 s^-1"             $ show [u| 1 s^-1 |]             @?= "[u| 1 s^-1 |]"
     , testCase "show 2 1 / kg s"         $ show [u| 2 1 / kg s |]         @?= "[u| 2 kg^-1 s^-1 |]"
     , testCase "show (1 % 2) kg"         $ show [u| 1 % 2 kg |]           @?= "[u| 0.5 kg |]"
+    ]
+  , testGroup "Basic operations"
+    [ testCase "2 + 2"                   $ [u| 2 s |] +: [u| 2 s |]        @?= [u| 4 s |]
+    , testCase "in m/s"                  $ inMetresPerSecond 5             @?= [u| 5 m/s |]
+    , testCase "polymorphic zero"        $ [u| 0 |] @?= [u| 0 m |]
+    , testCase "polymorphic frac zero"   $ [u| 0.0 |] @?= [u| 0.0 N / m |]
     ]
   , testGroup "showQuantity"
     [ testCase "myMass"         $ showQuantity myMass         @?= "65.0 kg"
